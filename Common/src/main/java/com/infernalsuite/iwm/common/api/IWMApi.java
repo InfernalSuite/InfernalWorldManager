1f0bf371c7b447590b1f50f07d6dfe2acf57a071
Philip Kelley <philip@thoriumcube.org>
2022-02-15 21:25:15 +0000

    Define API Implementation class

---
+++ Common/src/main/java/com/infernalsuite/iwm/common/api/IWMApi.java
@@ -0,0 +1,49 @@
+package com.infernalsuite.iwm.common.api;
+
+import com.infernalsuite.iwm.api.InfernalWorldManager;
+import com.infernalsuite.iwm.api.event.EventBus;
+import com.infernalsuite.iwm.api.event.EventDispatcher;
+import com.infernalsuite.iwm.api.loaders.LoaderRegistry;
+import com.infernalsuite.iwm.api.platform.Metadata;
+import com.infernalsuite.iwm.api.platform.Platform;
+import com.infernalsuite.iwm.api.platform.WorldAdapter;
+import com.infernalsuite.iwm.api.world.WorldRegistry;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class IWMApi implements InfernalWorldManager {
+
+    @Override
+    public @NonNull WorldRegistry getWorldRegistry() {
+        return null;
+    }
+
+    @Override
+    public @NonNull LoaderRegistry getLoaderRegistry() {
+        return null;
+    }
+
+    @Override
+    public @NonNull EventBus getEventBus() {
+        return null;
+    }
+
+    @Override
+    public @NonNull EventDispatcher getEventDispatcher() {
+        return null;
+    }
+
+    @Override
+    public @NonNull <T> WorldAdapter<T> getWorldAdapter(@NonNull Class<T> worldClass) {
+        return null;
+    }
+
+    @Override
+    public @NonNull Platform getPlatform() {
+        return null;
+    }
+
+    @Override
+    public @NonNull Metadata getMetadata() {
+        return null;
+    }
+}
