7b63736d4da0abc963ec2a513b01c1a49356a143
Philip Kelley <philip@thoriumcube.org>
2022-02-15 20:09:55 +0000

    Add methods for getting & putting chunk data

--- API/src/main/java/com/infernalsuite/iwm/api/formats/SerializedDataWrapper.java
+++ API/src/main/java/com/infernalsuite/iwm/api/formats/SerializedDataWrapper.java
@@ -4,4 +4,9 @@ package com.infernalsuite.iwm.api.formats;
  * A wrapper for the serialized world data stored by IWM
  */
 public interface SerializedDataWrapper {
+
+    byte[] getChunkByteArray(int x, int z);
+
+    void putChunkByteArray(int x, int z, byte[] serializedChunk);
+
 }
