0138e4934d7de5f2e604fbc09c9f448e4e96fe3b
Philip Kelley <philip@thoriumcube.org>
2022-02-16 16:35:37 +0000

    Implement obtain loader

--- Common/src/main/java/com/infernalsuite/iwm/common/loaders/IWMLoaderRegistry.java
+++ Common/src/main/java/com/infernalsuite/iwm/common/loaders/IWMLoaderRegistry.java
@@ -3,7 +3,9 @@ package com.infernalsuite.iwm.common.loaders;
 import com.infernalsuite.iwm.api.loaders.IWMLoader;
 import com.infernalsuite.iwm.api.loaders.LoaderRegistry;
 import com.infernalsuite.iwm.api.sources.DataSource;
+import com.infernalsuite.iwm.api.sources.type.*;
 import com.infernalsuite.iwm.common.event.IWMEventDispatcher;
+import com.infernalsuite.iwm.common.loaders.type.*;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.List;
@@ -59,11 +61,55 @@ public class IWMLoaderRegistry implements LoaderRegistry {
         });
     }
 
+    /**
+     * @implNote This method performs no caching of the obtained loader against the data source used to obtain it.
+     * This is by design, as a loader is bound to a data source. Loader names are auto-generated in the format using
+     * {@link DataSource#getName()} and appending "_loader". This name is stored in the backing loader map to allow retrieval.
+     * {@inheritDoc}
+     */
     @Override
     public @NonNull CompletableFuture<Optional<IWMLoader>> obtainLoader(@NonNull DataSource dataSource) {
         return CompletableFuture.supplyAsync(() -> {
             if (this.eventDispatcher.dispatchPreObtainLoader(dataSource)) return Optional.empty();
-            // TODO: Implement the actual matching of the DataSource to a loader
+            String newLoaderName = dataSource.getName().concat("_loader");
+            // TODO: Maybe we could change this to return the loader already in the map, but we first would need to validate the data source against the data source registry, to avoid non-deterministic behaviour
+            if (this.loaderMap.containsKey(newLoaderName)) throw new IllegalStateException("Already registered loader clashes with generated name: " + newLoaderName);
+            if (dataSource instanceof FileDS fileDataSource) {
+                FileLoader loader = new FileLoader(newLoaderName, fileDataSource);
+                if (!(this.eventDispatcher.dispatchPreRegisterLoader(newLoaderName, loader))) {
+                    this.loaderMap.put(newLoaderName, loader);
+                    this.eventDispatcher.dispatchPostRegisterLoader(newLoaderName, loader);
+                }
+                return Optional.ofNullable(this.eventDispatcher.dispatchPostObtainLoader(dataSource, loader));
+            } else if (dataSource instanceof MySQLDS mySqlDataSource) {
+                MySQLLoader loader = new MySQLLoader(newLoaderName, mySqlDataSource);
+                if (!(this.eventDispatcher.dispatchPreRegisterLoader(newLoaderName, loader))) {
+                    this.loaderMap.put(newLoaderName, loader);
+                    this.eventDispatcher.dispatchPostRegisterLoader(newLoaderName, loader);
+                }
+                return Optional.ofNullable(this.eventDispatcher.dispatchPostObtainLoader(dataSource, loader));
+            } else if (dataSource instanceof MongoDS mongoDataSource) {
+                MongoLoader loader = new MongoLoader(newLoaderName, mongoDataSource);
+                if (!(this.eventDispatcher.dispatchPreRegisterLoader(newLoaderName, loader))) {
+                    this.loaderMap.put(newLoaderName, loader);
+                    this.eventDispatcher.dispatchPostRegisterLoader(newLoaderName, loader);
+                }
+                return Optional.ofNullable(this.eventDispatcher.dispatchPostObtainLoader(dataSource, loader));
+            } else if (dataSource instanceof SeaweedDS seaweedDataSource) {
+                SeaweedLoader loader = new SeaweedLoader(newLoaderName, seaweedDataSource);
+                if (!(this.eventDispatcher.dispatchPreRegisterLoader(newLoaderName, loader))) {
+                    this.loaderMap.put(newLoaderName, loader);
+                    this.eventDispatcher.dispatchPostRegisterLoader(newLoaderName, loader);
+                }
+                return Optional.ofNullable(this.eventDispatcher.dispatchPostObtainLoader(dataSource, loader));
+            } else if (dataSource instanceof CouchbaseDS couchbaseDataSource) {
+                CouchbaseLoader loader = new CouchbaseLoader(newLoaderName, couchbaseDataSource);
+                if (!(this.eventDispatcher.dispatchPreRegisterLoader(newLoaderName, loader))) {
+                    this.loaderMap.put(newLoaderName, loader);
+                    this.eventDispatcher.dispatchPostRegisterLoader(newLoaderName, loader);
+                }
+                return Optional.ofNullable(this.eventDispatcher.dispatchPostObtainLoader(dataSource, loader));
+            }
             return Optional.empty();
         });
     }
