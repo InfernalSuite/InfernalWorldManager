63d119c6260a4acd90973bfe496451f0f2aa84fe
Philip Kelley <philip@thoriumcube.org>
2022-02-18 10:32:51 +0000

    Add NMS 1.18.1 Module
    Updated Git Ignore to exclude gradle output
    Added Gradle configuration for NMS module
    Added POM configuration for NMS module
    Updated root POM for NMS module

---
+++ IWM-NMS-v1_18_1/settings.gradle.kts
@@ -0,0 +1,7 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://papermc.io/repo/repository/maven-public/")
+    }
+}
+rootProject.name = "IWM-NMS-v1_18_1"
