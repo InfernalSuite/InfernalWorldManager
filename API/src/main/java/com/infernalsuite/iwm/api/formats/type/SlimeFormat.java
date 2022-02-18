bf6a30d7e487a223365ea67d7e4efb6295869791
Philip Kelley <philip@thoriumcube.org>
2022-02-18 01:53:59 +0000

    Add implementation for name method to SlimeFormat

--- API/src/main/java/com/infernalsuite/iwm/api/formats/type/SlimeFormat.java
+++ API/src/main/java/com/infernalsuite/iwm/api/formats/type/SlimeFormat.java
@@ -1,9 +1,10 @@
 package com.infernalsuite.iwm.api.formats.type;
 
 import com.infernalsuite.iwm.api.formats.Format;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 public interface SlimeFormat extends Format {
 
-
+    @Override default @NonNull String getName() { return "slime"; }
 
 }
