857d184a70dbb189973394e39d07b90a889fdf7c
Philip Kelley <philip@thoriumcube.org>
2022-02-26 09:39:44 +0000

    Add implementation for MySQL Data Source

--- Common/src/main/java/com/infernalsuite/iwm/common/sources/type/IwmMySqlDS.java
+++ Common/src/main/java/com/infernalsuite/iwm/common/sources/type/IwmMySqlDS.java
@@ -1,52 +1,56 @@
 package com.infernalsuite.iwm.common.sources.type;
 
 import com.infernalsuite.iwm.api.sources.type.MySQLDS;
+import lombok.Getter;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+@Getter
 public class IwmMySqlDS implements MySQLDS {
 
-    @Override
-    public @NonNull String getName() {
-        return null;
+    private final String name;
+    private final boolean enabled;
+    private final String sqlURL;
+    private final String host;
+    private final int port;
+    private final String database;
+    private final String username;
+    private final String password;
+    private final boolean useSSL;
+
+    public IwmMySqlDS(String name, boolean enabled, String host, String username, String password) {
+        this.name = name;
+        this.enabled = enabled;
+        this.host = host;
+        this.username = username;
+        this.password = password;
+        this.database = MySQLDS.super.getDatabase();
+        this.sqlURL = MySQLDS.super.getSqlUrl();
+        this.port = MySQLDS.super.getPort();
+        this.useSSL = MySQLDS.super.useSSL();
     }
 
-    @Override
-    public boolean isEnabled() {
-        return false;
+    public IwmMySqlDS(String name, boolean enabled, String sqlURL, String host, String portString, String database, String username, String password, String useSSLString) {
+        this.name = name;
+        this.enabled = enabled;
+        this.sqlURL = sqlURL == null ? MySQLDS.super.getSqlUrl() : sqlURL;
+        this.host = host;
+        this.port = portString == null ? MySQLDS.super.getPort() : Integer.parseInt(portString);
+        this.database = database == null ? MySQLDS.super.getDatabase() : database;
+        this.username = username;
+        this.password = password;
+        this.useSSL = useSSLString == null ? MySQLDS.super.useSSL() : Boolean.parseBoolean(useSSLString);
     }
 
-    @Override
-    public @NonNull String getSqlUrl() {
-        return MySQLDS.super.getSqlUrl();
+    public IwmMySqlDS(String name, boolean enabled, String sqlURL, String host, int port, String database, String username, String password, boolean useSSL) {
+        this.name = name;
+        this.enabled = enabled;
+        this.sqlURL = sqlURL;
+        this.host = host;
+        this.port = port;
+        this.database = database;
+        this.username = username;
+        this.password = password;
+        this.useSSL = useSSL;
     }
 
-    @Override
-    public @NonNull String getHost() {
-        return null;
-    }
-
-    @Override
-    public int getPort() {
-        return MySQLDS.super.getPort();
-    }
-
-    @Override
-    public @NonNull String getDatabase() {
-        return MySQLDS.super.getDatabase();
-    }
-
-    @Override
-    public @NonNull String getUsername() {
-        return null;
-    }
-
-    @Override
-    public @NonNull String getPassword() {
-        return null;
-    }
-
-    @Override
-    public boolean useSSL() {
-        return MySQLDS.super.useSSL();
-    }
 }
