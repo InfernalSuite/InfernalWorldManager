04d86574d6fb02e2bed66d94bdd13252feee39c2
Philip Kelley <philip@thoriumcube.org>
2022-02-26 13:51:09 +0000

    Add implementation for Mongo Data Source

--- Common/src/main/java/com/infernalsuite/iwm/common/sources/type/IwmMongoDS.java
+++ Common/src/main/java/com/infernalsuite/iwm/common/sources/type/IwmMongoDS.java
@@ -1,52 +1,55 @@
 package com.infernalsuite.iwm.common.sources.type;
 
 import com.infernalsuite.iwm.api.sources.type.MongoDS;
-import org.checkerframework.checker.nullness.qual.NonNull;
+import lombok.Getter;
 
+@Getter
 public class IwmMongoDS implements MongoDS {
 
-    @Override
-    public @NonNull String getName() {
-        return null;
+    private final String name;
+    private final boolean enabled;
+    private final String hostname;
+    private final int port;
+    private final String database;
+    private final String collection;
+    private final String username;
+    private final String password;
+    private final String authDB;
+
+    public IwmMongoDS(String name, boolean enabled, String hostname, String username, String password) {
+        this.name = name;
+        this.enabled = enabled;
+        this.hostname = hostname;
+        this.username = username;
+        this.password = password;
+        this.port = MongoDS.super.getPort();
+        this.database = MongoDS.super.getDatabase();
+        this.collection = MongoDS.super.getCollection();
+        this.authDB = MongoDS.super.getAuthDB();
     }
 
-    @Override
-    public boolean isEnabled() {
-        return false;
+    public IwmMongoDS(String name, boolean enabled, String hostname, String port_string, String database, String collection, String username, String password, String authDB) {
+        this.name = name;
+        this.enabled = enabled;
+        this.hostname = hostname;
+        this.port = port_string == null ? MongoDS.super.getPort() : Integer.parseInt(port_string);
+        this.database = database == null ? MongoDS.super.getDatabase() : database;
+        this.collection = collection == null ? MongoDS.super.getCollection() : collection;
+        this.username = username;
+        this.password = password;
+        this.authDB = authDB == null ? MongoDS.super.getAuthDB() : authDB;
     }
 
-    @Override
-    public @NonNull String getHostname() {
-        return null;
+    public IwmMongoDS(String name, boolean enabled, String hostname, int port, String database, String collection, String username, String password, String authDB) {
+        this.name = name;
+        this.enabled = enabled;
+        this.hostname = hostname;
+        this.port = port;
+        this.database = database;
+        this.collection = collection;
+        this.username = username;
+        this.password = password;
+        this.authDB = authDB;
     }
 
-    @Override
-    public int getPort() {
-        return MongoDS.super.getPort();
-    }
-
-    @Override
-    public @NonNull String getDatabase() {
-        return MongoDS.super.getDatabase();
-    }
-
-    @Override
-    public @NonNull String getCollection() {
-        return MongoDS.super.getCollection();
-    }
-
-    @Override
-    public @NonNull String getUsername() {
-        return null;
-    }
-
-    @Override
-    public @NonNull String getPassword() {
-        return null;
-    }
-
-    @Override
-    public @NonNull String getAuthDB() {
-        return MongoDS.super.getAuthDB();
-    }
 }
